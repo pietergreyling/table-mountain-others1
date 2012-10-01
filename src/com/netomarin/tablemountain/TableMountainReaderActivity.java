a2d3a64e0a2cb8c8757ea670cbc48a79de688461
Antonio Neto <netomarin@dhcp-172-31-119-33.sao.corp.google.com>
2012-10-01 16:47:19 -0300

    Initial commit

---
+++ src/com/netomarin/tablemountain/TableMountainReaderActivity.java
@@ -0,0 +1,20 @@
+package com.netomarin.tablemountain;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TableMountainReaderActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_mountain_reader);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_table_mountain_reader, menu);
+        return true;
+    }
+}
